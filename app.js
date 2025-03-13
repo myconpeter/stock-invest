40695287a25e5360ca7e86efcf029872f0d64b85
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2025-03-13 19:49:05 +0100

    jesus

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ dotenv.config();
 
 // crypto-apex is the new name
 
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 10000;
 //passport config:
 require('./config/cryptoPassport');
 require('./config/AdminPassport');
